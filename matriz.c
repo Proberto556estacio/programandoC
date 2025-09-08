85d35b56eba30473f09c70f58bc0ec1aa7cf96bb
Proberto556estacio <202507269518@alunos.estacio.br>
2025-09-08 12:24:43 -0300

    testando

--- matriz.c
+++ matriz.c
@@ -1,26 +1,22 @@
-# include <stdio.h>
-
-
-    int main() {
-
-        int numeros[] = {10, 20, 30, 40, 50};
-
-        /*
-        printf("O primeiro elemento é: %d\n", numeros[0]);
-        printf("O segundo elemento é: %d\n", numeros[1]);
-        printf("O terceiro elemento é: %d\n", numeros[2]);
-        printf("O quarto elemento é: %d\n", numeros[3]);
-        printf("O quinto elemento é: %d\n", numeros[4]);
-        */
-
-        for (int i = 0; i < 5; i++)
-        {
-            printf("Mostre os valores dos elementos: %d\n", numeros[i]); 
-        }
-        
-
-
-
-
-        return 0;
-    }
+#include <stdio.h>
+ 
+int main() {
+    int matriz[3][3];  // Declaração de uma matriz 3x3 de inteiros
+ 
+    // Inicializando os elementos da matriz
+    matriz[0][0] = 1;
+    matriz[0][1] = 2;
+    matriz[0][2] = 3;
+    matriz[1][0] = 4;
+    matriz[1][1] = 5;
+    matriz[1][2] = 6;
+    matriz[2][0] = 7;
+    matriz[2][1] = 8;
+    matriz[2][2] = 9;
+ 
+    // Acessando elementos da matriz
+    printf("O elemento na posição [0][0] é %d\n", matriz[0][0]);
+    printf("O elemento na posição [1][1] é %d\n", matriz[1][1]);
+ 
+    return 0;
+}
---
+++ vetornotas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main () {
+
+    char letras[4] = {'A', 'B', 'C'};
+
+    printf("Primeira letra: %c\n", letras[0]);
+    printf("Segunda letra: %c\n", letras[1]);
+
+
+
+
+
+
+
+    return 0;
+
+}
