6ad2e6a4b1d0fe6f0f5d3ac2751330f83a2c3071
Proberto556estacio <202507269518@alunos.estacio.br>
2025-09-08 12:47:13 -0300

    CODAR

---
+++ codarvetromatriz.c
@@ -0,0 +1,31 @@
+# include <stdio.h>
+
+int main() {
+
+    int index;
+
+     char *nomeAlunos = {
+            {"Aluno 0", "Pt: 30", "Mat: 90"},
+            {"Aluno 1", "Pt: 60", "Mat: 60"},
+            {"Aluno 2", "Pt: 90", "Mat: 30"}
+        };
+
+        //Antes de acessar esses valores vamos criar um prompt para guiar o usuário
+        printf("Digite o número do aluno para mostrar as notas...\n");
+        printf("Aluno 0, digite 0\n");
+        printf("Aluno 1, digite 1\n");
+        printf("Aluno 2, digite 2\n");
+
+        printf("Voce escolheu - Aluno 1\n");
+        
+        scanf("%d, index");
+
+        printf("As notas do %s são: %s, %s...\n", nomesAlunos[index][0], nomesAlunos[index][1], nomesAlunos[index][2]);
+
+
+
+
+
+    return 0;
+
+}
